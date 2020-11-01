67b5d4525cf438ab4b2100c7c81ec00efe6ba68b
free2yogesh <73719402+free2yogesh@users.noreply.github.com>
2020-11-01 23:59:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-customhost = "emp-db.cdrwuou9xf7j.us-east-1.rds.amazonaws.com"
+customhost = "ys-proj-emp-db.cdrwuou9xf7j.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin1234"
 customdb = "employee"
-custombucket = "addemployee"
 customregion = "us-east-1"
+custombucket = "addemployee"
+
